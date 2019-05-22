a7df4d014d3c0b3af00d6fde8f35efa682b9012f
lziz <840432648@qq.com>
2019-05-22 16:01:19 +0800

    请开始你的表演

---
+++ mall/src/main/java/com/cskaoyan/mall/MallApplication.java
@@ -0,0 +1,16 @@
+package com.cskaoyan.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ */
+@SpringBootApplication
+public class MallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallApplication.class, args);
+    }
+
+}
